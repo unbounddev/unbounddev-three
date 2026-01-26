e5d21c9ccb4055444e31b5e0fc09f94627e480df
unbounddev <unbounddev@git>
2026-01-25 18:33:05 -0800

    Add STANDARD_MAPPING for gamepads

--- src/input.js
+++ src/input.js
@@ -2,6 +2,34 @@
 
 // Vendor: 2dc8 Product: 310a 8BitDo Ultimate 2C 
 
+const STANDARD_MAPPING = {
+    BUTTONS: {
+        A: 0,
+        B: 1,
+        X: 2,
+        Y: 3,
+        LB: 4,
+        RB: 5,
+        LT: 6,
+        RT: 7,
+        CENTER_LEFT: 8,
+        CENTER_RIGHT: 9,
+        LS: 10,
+        RS: 11,
+        UP: 12,
+        DOWN: 13,
+        LEFT: 14,
+        RIGHT: 15,
+        CENTER: 16
+    },
+    AXES: {
+        LSX: 0,
+        LXY: 1,
+        RSX: 2,
+        RSY: 3
+    }
+}
+
 
 export const EIGHT_BITDO_ULTIMATE_2C_MAPPING = {
     "A": 0,
